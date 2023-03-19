3f18607becafa5e0ddb93d5dc4efd167918723f3
Rodrigo Moran <rigo.moran0@gmail.com>
2023-03-19 13:13:22 -0600

    fix: add hashnode social icon

--- themes/tien/layouts/partials/icons.sh
+++ themes/tien/layouts/partials/icons.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
-icons="twitter github-alt linkedin stack-overflow"
-dest=fontawesome
+: ${icons:=twitter}
+dest="../../static/fontawesome"
 url=https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/
 mkdir -p "${dest}"
 for icon in $icons; do
